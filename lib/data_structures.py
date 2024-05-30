927456ae33226148b411abc5789e61afeabc0853
MartinWakaba <martin.wakaba@student.moringaschool.com>
2024-05-31 00:11:29 +0300

    done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,4 @@
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +18,37 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food['heat_level'] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        spicy_hot_emoji = "🌶" * food['heat_level']
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {spicy_hot_emoji}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food['cuisine'].lower() == cuisine.lower():
+            return food
+    return None    
 
-def print_spiciest_foods(spicy_foods):
-    pass
+def print_spiciest_foods(spicy_foods, spicy_limits = 5):
+    for food in spicy_foods:
+        if food['heat_level'] > spicy_limits:
+            spicy_hot_emoji = "🌶" * food['heat_level']
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {spicy_hot_emoji}")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat = sum(dish['heat_level'] for dish in spicy_foods)
+    num_dishes = len(spicy_foods)
+
+    if num_dishes > 0:
+        return total_heat // num_dishes
+    else:
+        return 0
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
